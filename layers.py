4f7a371908470d50c5ecbd6582b756e29992f45b
pyfra <f.defano1@gmail.com>
2020-02-22 15:04:22 +0100

    first commit layers.py

---
+++ layers.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+
+class Layer:
+    """
+    A building block. Each layer is capable of performing two things:
+
+    - Process input to get output:           output = layer.forward(input)
+
+    - Propagate gradients through itself:    grad_input = layer.backward(input, grad_output)
+
+    Some layers also have learnable parameters which they update during layer.backward.
+    """
+
+    def __init__(self):
+        """Here you can initialize layer parameters (if any) and auxiliary stuff."""
+        # A dummy layer does nothing
+        pass
+
+    def forward(self, input):
+        """
+        Takes input data of shape [batch, input_units], returns output data [batch, output_units]
+        """
+        # A dummy layer just returns whatever it gets as input.
+        return input
+
+    def backward(self, input, grad_output):
+        """
+        Performs a backpropagation step through the layer, with respect to the given input.
+
+        To compute loss gradients w.r.t input, you need to apply chain rule (backprop):
+
+        d loss / d x  = (d loss / d layer) * (d layer / d x)
+
+        Luckily, you already receive d loss / d layer as input, so you only need to multiply it by d layer / d x.
+
+        If your layer has parameters (e.g. dense layer), you also need to update them here using d loss / d layer
+        """
+        # The gradient of a dummy layer is precisely grad_output, but we'll write it more explicitly
+        num_units = input.shape[1]
+
+        d_layer_d_input = np.eye(num_units)
+
+        return np.dot(grad_output, d_layer_d_input)  # chain rule
+
+
+class ReLU(Layer):
+    def __init__(self):
+        pass
+
+    def forward(self, input):
+        return np.maximum(input, 0)
+
+    def backward(self, input, grad_output):
+        relu_grad = input > 0
+        return grad_output * relu_grad
+
+
+class Dense(Layer):
+    def __init__(self, input_units, output_units, learning_rate=0.1):
+        """
+        A dense layer is a layer which performs a learned affine transformation:
+        f(x) = <W*x> + b
+        """
+        self.learning_rate = learning_rate
+        self.weights = np.random.randn(input_units, output_units) * 0.01
+        self.biases = np.zeros(output_units)
+
+    def forward(self, input):
+        """
+        Perform an affine transformation:
+        f(x) = <W*x> + b
+
+        input shape: [batch, input_units]
+        output shape: [batch, output units]
+        """
+        return input @ self.weights + self.biases
+
+    def backward(self, input, grad_output):
+        # compute d f / d x = d f / d dense * d dense / d x
+        # where d dense/ d x = weights transposed
+        grad_input = np.dot(grad_output, self.weights.T)
+        # compute gradient w.r.t. weights and biases
+        grad_weights = np.dot(input.T, grad_output) / input.shape[0]
+        grad_biases = np.sum(grad_output, axis=0)
+
+        assert grad_weights.shape == self.weights.shape and grad_biases.shape == self.biases.shape
+        self.weights = self.weights - self.learning_rate * grad_weights
+        self.biases = self.biases - self.learning_rate * grad_biases
+
+        return grad_input
